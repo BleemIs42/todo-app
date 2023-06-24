52bf0d3feba542502945511056a57805e6572c83
Barry Yan <barry.yansh@gmail.com>
2023-06-24 22:38:13 +0800

    Add todo-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useState } from "react";
 import "./App.css"
-import { getId } from "./utils";
-import { TodoItem } from "./interface";
-
-interface InputProps {
-  onEnter: (text: string) => void
-}
-
-const InputWrapper = ({ onEnter }: InputProps) => {
-  const [value, setValue] = useState("");
-  const onKeyup = (e: any) => {
-    if (e.key === "Enter") {
-      onEnter(e.target.value);
-      setValue("");
-    }
-  }
-  return <input className="input"
-    value={value}
-    onChange={e => setValue(e.target.value)}
-    onKeyUp={onKeyup} />
-}
 
 export default () => {
-  const [todoList, setTodoList] = useState<TodoItem[]>([]);
-  const onEnter = (text: string) => {
-    setTodoList([
-      ...todoList,
-      {
-        id: getId(),
-        text,
-        isDone: false,
-      }])
-  }
 
   return <div className="wrap">
     <div className="header">TodoList</div>
     <div className="container">
-      <InputWrapper onEnter={onEnter} />
-      <div className="list">
-        {
-          todoList.map(todo => <div>{ todo.text }</div>)
-        }
-      </div>
+    <input className="input" />
     </div>
   </div>
 }
