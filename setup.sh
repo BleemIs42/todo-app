f9d46f3ac7150540e0b847850e38cbd7d2527f05
Barry Yan <barry.yansh@gmail.com>
2023-06-29 21:49:24 +0800

    Skip install node if it exist

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# install node
-brew install node
+if ! node -v &>/dev/null
+then
+  echo "Start install node"
+  brew install node
+fi
 
-# init react project 
+# init react project
 yes | npm create vite todo-app -- --template react-ts
 
 # install dependence
